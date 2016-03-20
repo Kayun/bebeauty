62175047670582cbc3974a05f8b362b71e6e9853
Artyom Kayun <kayun.artem@gmail.com>
2016-03-20 18:58:03 +0600

    chore (copy.js) add copy:data task Задача копирует содержимое папки /src/resources/data в деректорию /public

--- builder/tasks/copy.js
+++ builder/tasks/copy.js
@@ -1,8 +1,28 @@
 import gulp from 'gulp';
 
-gulp.task('copy', done => {
-  gulp.src('src/resources/**/*')
+const resources = done => {
+  gulp.src(['src/resources/**/*', '!src/resources/data/**/*'])
     .pipe(gulp.dest('public'));
 
   done();
-});
+};
+
+const data = done => {
+  gulp.src('src/resources/data/**/*', {base: 'src/resources'})
+    .pipe(gulp.dest('public'));
+
+  done();
+};
+
+gulp.task('copy:resources', resources);
+gulp.task('copy:data', data);
+
+gulp.task('copy', gulp.parallel(
+  'copy:resources',
+  'copy:data'
+));
+
+export default {
+  resources,
+  data
+};
